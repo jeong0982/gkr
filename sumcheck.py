8d17baa57bdb248fe34c4d34737172c47083c16f
jeong0982 <jeong0982@kaist.ac.kr>
2022-11-01 16:59:28 +0900

    Fix type hint

--- sumcheck.py
+++ sumcheck.py
@@ -1,24 +1,7 @@
 from ethsnarks import field
 from poly import *
 from util import *
-
-def generate_binary_strings(bit_count):
-    binary_strings = []
-
-    def genbin(n, bs=""):
-        if len(bs) == n:
-            binary_strings.append(bs)
-        else:
-            genbin(n, bs + "0")
-            genbin(n, bs + "1")
-
-    genbin(bit_count)
-    return binary_strings
-
-def Convert(string):
-    list1 = []
-    list1[:0] = string
-    return list1
+from typing import Callable
 
 # TODO
 # FS transform
@@ -45,9 +28,9 @@ def verify_sumcheck(claims, p: list[list[field.FQ]], g, v: int):
 
     return True
 
-def sumcheck(c: field.FQ, g, v):
+def sumcheck(c: field.FQ, g: Callable[[list[field.FQ]], field.FQ], v):
 
-    if(v == 1 and (g([0]) + g([1])) == c):
+    if(v == 1 and (g([field.FQ.zero()]) + g([field.FQ.one()])) == c):
         return True, []
 
     g_vector = [field.FQ(0)] * v
@@ -56,12 +39,7 @@ def sumcheck(c: field.FQ, g, v):
     # first round
     # g1(X1)=∑(x2,⋯,xv)∈{0,1}^v g(X_1,x_2,⋯,x_v)
     def g_1(x_1):
-        assignment = generate_binary_strings(v - 1)
-        for i in range(len(assignment)):
-            assignment[i] = Convert(assignment[i])
-            for j in range(len(assignment[i])):
-                assignment[i][j] = field.FQ(int(assignment[i][j]))
-
+        assignment = generate_binary(v - 1)
         for i in range(len(assignment)):
             assignment[i].insert(0, x_1)
 
@@ -78,13 +56,8 @@ def sumcheck(c: field.FQ, g, v):
         g_vector[0] = g_1(r[0])
 
     for j in range(1, v - 1): # 1 < j < v round
-        def g_j(x):
-            assignment = generate_binary_strings(v - j - 1)
-            for i in range(len(assignment)):
-                assignment[i] = Convert(assignment[i])
-                for k in range(len(assignment[i])):
-                    assignment[i][k] = field.FQ(int(assignment[i][k]))
-
+        def g_j(x: field.FQ):
+            assignment = generate_binary(v - j - 1)
             for i in range(len(assignment)):
                 assignment[i] = r[0 : j] + [x] + assignment[i]
 
@@ -93,7 +66,7 @@ def sumcheck(c: field.FQ, g, v):
                 output += g(assignment[i]) 
             return output
 
-        if g_vector[j - 1] != (g_j(0) + g_j(1)):
+        if g_vector[j - 1] != (g_j(field.FQ.zero()) + g_j(field.FQ.one())):
             return False, []
         else:
             r[j] = field.FQ.random()
