91392a362cbb09621fa8cb257eff4f9a712c8342
jeong0982 <jeong0982@kaist.ac.kr>
2023-02-06 17:26:07 +0900

    Cleanup

--- rust/src/gkr/sumcheck.rs
+++ rust/src/gkr/sumcheck.rs
@@ -4,6 +4,7 @@ use ff::PrimeField;
 use itertools::Itertools;
 use mimc_rs::{Fr, FrRepr, Mimc7};
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+
 use super::poly::*;
 
 pub fn convert_s_to_fr<S>(v: &S) -> mimc_rs::Fr
@@ -50,7 +51,6 @@ pub fn prove_sumcheck_opt<S: PrimeField<Repr = [u8; 32]> + std::hash::Hash>(
         .par_iter()
         .map(|assignment| {
             let mut add_1_sub = add_i.clone();
-
             let f2_1_sub = partial_eval_from(f2.clone(), assignment, 2);
             let f1_1_sub = partial_eval_from(f1.clone(), assignment, 2);
             let add_1_sub = partial_eval_from_binary_form(&add_i.clone(), assignment, 2);
